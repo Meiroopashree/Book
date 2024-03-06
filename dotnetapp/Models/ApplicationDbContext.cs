a96a5deb624ac45a5d1ba5b06f8ea15a2ede1c7f
neojarvis <techadmin@iamneo.ai>
2024-03-06 02:20:57 +0000

    Auto-save-commit-06/03/2024--07:50:55

--- dotnetapp/Models/ApplicationDbContext.cs
+++ dotnetapp/Models/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using dotnetapp.Models;
 
 public class ApplicationDbContext : DbContext
 {
--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -1,16 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using dotnetapp.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using dotnetapp.Models;
+using dotnetapp.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
+
+builder.Services.AddSingleton<BookRepository>();
+builder.Services.AddSingleton<OrderRepository>();
+builder.Services.AddSingleton<IBookService, BookService>();
+builder.Services.AddSingleton<IOrderService, OrderService>();
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin",
+        builder => builder.WithOrigins()
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -20,6 +45,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("AllowOrigin");
 
 app.MapControllers();
 
